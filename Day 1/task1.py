aac330d991626e88dd9878f5fe137d1dbf640fee
Martin Kobro <martin.kobro@vegvesen.no>
2025-12-01 11:22:17 +0100

    Puzzle 1, Done

---
+++ Day 1/task1.py
@@ -0,0 +1,26 @@
+
+# Read input
+with open('input_task_1', 'r') as file:
+    rotations = file.readlines();
+
+# Initialize variables
+position = 50
+counter = 0
+
+for rotation in rotations:
+    direction, value = rotation[0], int(rotation[1:])
+
+    if direction == 'R':
+        position += value
+    elif direction == 'L':
+        position -= value
+
+    # Wrap around logic (0...99)
+    position = position % 100
+
+    # Check for zero condition
+    if position == 0:
+        counter += 1
+
+
+print(counter)
